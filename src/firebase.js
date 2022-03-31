052ca130d6c7b863cf600441059d234b504cfb42
hsangwon <101050184+hsangwon@users.noreply.github.com>
2022-03-31 19:11:52 +0900

    finish homework

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDwV34IbMccJ1SoORD0qmY8_AAOJ7oq3oI",
-  authDomain: "hanghae-react-8ca3a.firebaseapp.com",
-  projectId: "hanghae-react-8ca3a",
-  storageBucket: "hanghae-react-8ca3a.appspot.com",
-  messagingSenderId: "836610416958",
-  appId: "1:836610416958:web:6e48c4308d56e62bc18c6f",
-  measurementId: "G-Z2086CJXM6",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
